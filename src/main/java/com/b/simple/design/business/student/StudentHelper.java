af88f5d5d769a6c59e774d4fecf9bcd9e2357c6b
Alex Shamrai <gibsonshamray@gmail.com>
2023-12-15 17:23:23 +0200

    Refactor StudentHelper

--- src/main/java/com/b/simple/design/business/student/StudentHelper.java
+++ src/main/java/com/b/simple/design/business/student/StudentHelper.java
@@ -1,12 +1,22 @@
 package com.b.simple.design.business.student;
 public class StudentHelper {
 
-	/* PROBLEM 1 */	
+	public static final int GRADE_B_LOWER_BOUNDARY = 51;
+	public static final int GRADE_B_UPPER_BOUNDARY = 80;
+	public static final int EXTRA_INCREASE_FOR_MATH = 10;
+	public static final int A_GRADE_LIMIT = 91;
+	public static final int B_GRADE_LIMIT = 51;
+	public static final int MATHS_EXTRA_ADDITION = 5;
+	public static final int GOOD_MARK_BOUNDARY = 80;
+	public static final int BAD_MARK_BOUNDARY = 20;
+
 	/*
 	* You get a grade B if marks are between 51 and 80 (both inclusive). Except for Maths where the upper limit is increased by 10.
 	*/
 	public boolean isGradeB(int marks, boolean isMaths) {
-		return isMaths ? marks>=51 && marks<=90 : marks>=51 && marks<=80; 
+		int extraIncrease = isMaths ? EXTRA_INCREASE_FOR_MATH : 0;
+		int upperBoundary = GRADE_B_UPPER_BOUNDARY + extraIncrease;
+		return marks >= GRADE_B_LOWER_BOUNDARY && marks <= upperBoundary;
 	}
 
 	/* PROBLEM 2 */
@@ -17,26 +27,12 @@ public class StudentHelper {
 	*/
 
 	public String getGrade(int mark, boolean isMaths) {
-		String grade = "C";
-		
-		if (isGradeA(mark, isMaths))
-			grade = "A";
-		else if (isBGrade(mark, isMaths)) {
-			grade = "B";
-		}
-		return grade;
-	}
-
-	private boolean isGradeA(int mark, boolean isMaths) {
-		int lowerLimitForAGrade = isMaths ? 95
-				: 90;
-		return mark > lowerLimitForAGrade;
-	}
-
-	private boolean isBGrade(int mark, boolean isMaths) {
-		int lowerLimitGradeB = isMaths ? 55
-				: 50;
-		return mark > lowerLimitGradeB && mark < 90;
+		int extraLimitForMaths = isMaths ? MATHS_EXTRA_ADDITION : 0;
+		if (mark >= A_GRADE_LIMIT + extraLimitForMaths)
+			return "A";
+		if (mark >= B_GRADE_LIMIT + extraLimitForMaths)
+			return "B";
+		return "C";
 	}
 
     /*  PROBLEM 3
@@ -55,12 +51,22 @@ public class StudentHelper {
      * marks2 - your friends marks
     */
         
-    public String willQualifyForQuiz(int marks1, int marks2, boolean isMaths) {
-        if ((isMaths ? marks1 <= 25 : marks1 <= 20)
-                || (isMaths ? marks2 <= 25 : marks2 <= 20)) return "NO";
-        if ((isMaths ? marks1 >= 85 : marks1 >= 80)
-                || (isMaths ? marks2 >= 85 : marks2 >= 80)) return "YES";
+    public String willQualifyForQuiz(int yourMarks, int yourFriendsMarks, boolean isMaths) {
+        if (isBadInSubject(yourMarks, isMaths) || isBadInSubject(yourFriendsMarks, isMaths))
+			return "NO";
+        if (isGoodInSubject(yourMarks, isMaths) || isGoodInSubject(yourFriendsMarks, isMaths))
+			return "YES";
         return "MAYBE";
-    }	
+    }
+
+	private static boolean isGoodInSubject(int marks, boolean isMaths) {
+		int extraLimitForMaths = isMaths ? MATHS_EXTRA_ADDITION : 0;
+		return marks >= GOOD_MARK_BOUNDARY + extraLimitForMaths;
+	}
+
+	private static boolean isBadInSubject(int marks, boolean isMaths) {
+		int extraLimitForMaths = isMaths ? MATHS_EXTRA_ADDITION : 0;
+		return marks <= BAD_MARK_BOUNDARY + extraLimitForMaths;
+	}
 
 }
